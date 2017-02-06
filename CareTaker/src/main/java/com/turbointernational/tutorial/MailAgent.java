1dd94f2a4a4b6657cd1b571a924fe83916da92b0
kshakirov <kshakirov@zoral.com.ua>
2017-02-06 15:07:20 +0200

    mail in work

--- CareTaker/src/main/java/com/turbointernational/tutorial/MailAgent.java
+++ CareTaker/src/main/java/com/turbointernational/tutorial/MailAgent.java
@@ -15,32 +15,17 @@ import javax.mail.Message;
 public class MailAgent {
     public static void main(String[] args) {
         Email email = new Email();
-        email.setFromAddress("lollypop", "lolly.pop@mymail.com");
+        email.setFromAddress("kirill", "kyrylo.shakirov@zorallabs.com");
         email.setReplyToAddress("lollypop", "lolly.pop@othermail.com");
-        email.addRecipient("lollypop", "lolly.pop@somemail.com", Message.RecipientType.TO);
-        email.addRecipient("C. Cane", "candycane@candyshop.org", Message.RecipientType.TO);
-        email.addRecipient("C. Bo", "chocobo@candyshop.org", Message.RecipientType.CC);
+        email.addRecipient("Kirill Shakirov", "kirill.shakirov4@gmail.com", Message.RecipientType.TO);
         email.setSubject("hey");
         email.setText("We should meet up! ;)");
         email.setTextHTML("&lt;img src=&#39;cid:wink1&#39;&gt;&lt;b&gt;We should meet up!&lt;/b&gt;&lt;img src=&#39;cid:wink2&#39;&gt;");
-        //email.addEmbeddedImage("wink1", imageByteArray, "image/png");
-        //email.addEmbeddedImage("wink2", imageDatesource);
-        //email.addAttachment("invitation", pdfByteArray, "application/pdf");
-        //email.addAttachment("dresscode", odfDatasource);
 
-        //email.signWithDomainKey(privateKeyData, "somemail.com", "selector");
+        Mailer mailer = new
 
-        new
-
-                Mailer(
-                new ServerConfig("smtp.host.com", 587, "user@host.com", "password"),
-
-                TransportStrategy.SMTP_TLS,
-                new
-
-                        ProxyConfig("socksproxy.host.com", 1080, "proxy user", "proxy password")
-        ).
-
-                sendMail(email);
+                Mailer("smtp.office365.com", 587, "kyrylo.shakirov@zorallabs.com", "",                TransportStrategy.SMTP_TLS);
+                mailer.sendMail(email);
+                mailer.validate(email);
     }
 }
